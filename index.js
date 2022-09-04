20fab5172df56eab04bccaa2d7bec32f80b5afc2
CaoHuuKhuongDuy <khuongduy2121@gmail.com>
2022-09-04 19:42:13 +0700

    modify

--- index.js
+++ index.js
@@ -3,23 +3,7 @@ const bodyParser = require('body-parser')
 const express = require('express');
 const cors = require('cors');
 const app = express();
-// const mongoose = require('mongoose')
-// const My_Uri = "mongodb+srv://CaoHuuKhuongDuy:21nhatranG@cluster0.hrxneeb.mongodb.net/fcc-mongodb-and-mongoose?retryWrites=true&w=majority"
-// mongoose.connect(My_Uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-
-// Basic Configuration
 const port = process.env.PORT || 3000;
-
-// const LinkSchema = new mongoose.Schema({
-//   url : String
-// })
-// let Url_model = mongoose.model('Url',LinkSchema);
-
-// let url = new Url_model({url : "https://www.facebook.com"})
-// url.save()
-
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -44,10 +28,10 @@ function valid_url(string) {
   
   try {
     url = new URL(string);
-  } catch (_) {
+  }
+  catch (_) {
     return false;  
   }
-
   return url.protocol === "http:" || url.protocol === "https:";
 }
 
