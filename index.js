0f78fc478d7ddc4165623bd84aa3de232bd697e9
CaoHuuKhuongDuy <khuongduy2121@gmail.com>
2022-09-04 16:28:36 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
+const bodyParser = require('body-parser')
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose')
+const My_Uri = "mongodb+srv://CaoHuuKhuongDuy:21nhatranG@cluster0.hrxneeb.mongodb.net/fcc-mongodb-and-mongoose?retryWrites=true&w=majority"
+mongoose.connect(My_Uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -19,6 +26,16 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/youtube',function(req, res){
+  res.redirect("https://www.youtube.com/");
+})
+// app.get("/hello",function (req,res){
+//   res.send("hello")
+// })
+app.post('/api/shorturl',function (req, res){
+  // console.log(req.body.url)
+  res.send("hello");
+})
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
